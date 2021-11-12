29d0bf1f1c152164afb57ba39389d4fef2b6ac2d
RijalArul <fakhrulmrijal7599@gmail.com>
2021-11-12 08:53:16 +0700

    reusable card country component

---
+++ client/src/components/CardCountry.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="card" style="width: 18rem; height: 350px">
+      <img
+        class="card-img-top"
+        :src="area.ensignUrl"
+        alt="Card image cap"
+        style="height: 380px"
+      />
+      <div class="card-body">
+        <p class="card-text">
+          {{ area.name }}
+        </p>
+        <button class="btn btn-info" @click.prevent="clickAllClub(area.id)">
+          All Club {{ area.name }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardCountry",
+  props: ["area"],
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -28,25 +28,7 @@
             v-for="area in areas"
             :key="area.id"
           >
-            <div class="card" style="width: 18rem; height: 350px">
-              <img
-                class="card-img-top"
-                :src="area.ensignUrl"
-                alt="Card image cap"
-                style="height: 380px"
-              />
-              <div class="card-body">
-                <p class="card-text">
-                  {{ area.name }}
-                </p>
-                <button
-                  class="btn btn-info"
-                  @click.prevent="clickAllClub(area.id)"
-                >
-                  All Club {{ area.name }}
-                </button>
-              </div>
-            </div>
+            <CardCountry :area="area" />
           </div>
         </div>
       </div>
@@ -55,9 +37,13 @@
 </template>
 
 <script>
+import CardCountry from "../components/CardCountry.vue";
 export default {
   name: "Home",
-  components: {},
+  props: ["area"],
+  components: {
+    CardCountry,
+  },
   created() {
     this.fetchAreas();
   },
