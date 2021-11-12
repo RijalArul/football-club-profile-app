2c00607d53c8ccb37ba8ca893dc8214346e7da66
RijalArul <fakhrulmrijal7599@gmail.com>
2021-11-12 09:35:09 +0700

    Updated reusable card all club

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # football-club-profile-app
+
+Tech STack / Tools 
+
+Vue.js
+Vuex
+localStorage
+3rd party API datafootball
+
+Description Project
+
+Football App is web app that we can see profile club by europe continent. In here we can see Europe Country, All Club per country and club profile.
+
+Soon Add Feature.
+
+I will add feature like match upcoming, comptetition and standing. Also will make  a beautiful app.
+
---
+++ client/src/components/ListSquadTable.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <th scope="row">{{ i + 1 }}</th>
+    <td>{{ teamSquad.name }}</td>
+    <td>{{ teamSquad.position }}</td>
+    <td>{{ teamSquad.nationality }}</td>
+    <td>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-toggle="modal"
+        data-target="#exampleModal"
+        @click.prevent="fetchPlayerProfile(teamSquad.id)"
+      >
+        {{ teamSquad.name }}
+      </button>
+
+      <div
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                {{ teamSquad.name }}
+              </h5>
+              <button
+                type="button"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="float-right">
+                <h5>FIrst Name : {{ player.firstName }}</h5>
+                <h5>Position : {{ player.position }}</h5>
+                <h5>Nationality : {{ player.nationality }}</h5>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                data-dismiss="modal"
+                class="btn btn-primary"
+              >
+                Back
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </td>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["teamSquad", "i"],
+  computed: {
+    club() {
+      return this.$store.state.club;
+    },
+
+    squad() {
+      return this.$store.state.squad;
+    },
+
+    player() {
+      return this.$store.state.player;
+    },
+  },
+  methods: {
+    fetchPlayerProfile(id) {
+      this.$store.dispatch("actionPlayerProfile", id);
+    },
+  },
+  created() {
+    console.log("MASUK");
+  },
+};
+</script>
